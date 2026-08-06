5fdd4b2bcb43e190d584062277a885a59b2351ab
Sakib <csakib049@gmail.com>
2026-08-06 23:30:54 +0600

    practicing ts

---
+++ Practice/user.js
@@ -0,0 +1,9 @@
+"use strict";
+var username = "sakib";
+var Name = "shahriar";
+var age = 16;
+var a = true;
+console.log(a);
+console.log(username);
+console.log(age);
+console.log(Name);
---
+++ Practice/user.ts
@@ -0,0 +1,13 @@
+var username : string = "sakib";
+
+var Name : string = "shahriar";
+
+var age : number = 16;
+
+var a:boolean = true;
+
+
+console.log(a);
+console.log(username);
+console.log(age);
+console.log(Name);
