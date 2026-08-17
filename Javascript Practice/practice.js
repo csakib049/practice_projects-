b7bfc7e97e8fd3ac2bb15a9f378fd9a9177ae6c2
Sakib <csakib049@gmail.com>
2026-08-17 18:23:55 +0600

    practicing js-2

--- Javascript Practice/practice.js
+++ Javascript Practice/practice.js
@@ -1,1 +1,13 @@
-console.log("sakib");
+let num =63;
+
+if(80<=num && num<=100){
+    console.log("A");
+}else if (70<=num && num<=79){
+    console.log("B");
+}else if(60<=num && num<=69){
+    console.log("C");
+}else if(50<=num && num<=59){
+    console.log("D");
+}else {
+    console.log("F");
+}
