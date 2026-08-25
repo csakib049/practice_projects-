4feaea5c08699256eb1698d020abd7446ac1af84
Sakib <csakib049@gmail.com>
2026-08-25 23:23:21 +0600

    practicing DOM

--- Javascript Practice/practice.js
+++ Javascript Practice/practice.js
@@ -1,1 +1,32 @@
-console.log("sakib")
+// let btn1 = document.querySelector("#btn1");
+
+
+// console.log(btn1);
+// console.dir(btn1);
+
+// console.log(btn1.innerText);
+
+
+
+
+// // let div1=document.querySelector("#div1");
+
+// // console.log(div1.innerHTML);
+
+// // console.log(div1.innerText);
+
+
+let div1= document.querySelector("#div1");
+
+console.log(div1.innerHTML);
+console.log(div1.innerText);
+
+
+
+let nav= document.querySelector("#nav");
+
+
+nav.innerText = nav.innerText + " sakib sakib ...";
+
+
+console.log(nav.innerText);
