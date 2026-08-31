c5f45c2ec1e6208b0a1e16473e4053ae9ca871e1
Sakib <csakib049@gmail.com>
2026-08-31 07:20:19 +0600

    puzzle

--- Javascript Practice/practice.js
+++ Javascript Practice/practice.js
@@ -1,87 +1,14 @@
-// // let div1 = document.querySelector("#div1");
+myFun();//-------->second
 
-// // console.log(div1);
-
-
-
-// // console.log(div1.setAttribute("name","mojo"));
-
-
-
-// let div1 = document.querySelector("#div1");
-
-// console.log(div1);
-
-// console.log(div1.setAttribute("name","mojo_Sakib"));
-
-
-
-// let newBtn = document.createElement("button");
-
-// newBtn.innerText="chipoo"
-
-// console.log(newBtn);
-
-
-
-
-
-
-// let newbtn = document.createElement("button");
-
-// newbtn.innerText="sakibb";
-
-// console.log(newbtn);
-
-
-
-// let div1 = document.querySelector("#div1")
-
-
-// div1.append(newbtn);
-
-
-// let newbtn = document.createElement("button");
-
-// newbtn.innerText = "dabadooo";
-
-// console.log(newbtn);
-
-// let div1 = document.querySelector("#div1");
-
-// div1.prepend(newbtn);
-
-
-
-
-// let btn1 = document.querySelector("#btn1");
-
-// btn1.onclick=()=>{
-//     console.log("btn1 was clicked ");
-
-// }
-
-
-// let div1 = document.querySelector("#box1");
-
-// div1.onmouseover = (e)=>{
-//     console.log(e);
-
-//     console.log("hoveringg..")
-// }
-
-
-
-
-function calculate(a,b){
-    return a+b;
+var myFun = function (){
+    console.log("First");
 }
 
+myFun(); //-----------> First
 
 
+function myFun(){
+    console.log("Second");
+}
 
-let a = 234;
-
-const b =234;
-
-console.log(calculate(a,b));
+myFun(); //-----> First 
