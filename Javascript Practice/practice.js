83282f6d654c0a0d5899357549407ce7c5ea74bd
Sakib <csakib049@gmail.com>
2026-09-01 08:05:18 +0600

    IIFE

--- Javascript Practice/practice.js
+++ Javascript Practice/practice.js
@@ -1,14 +1,33 @@
-myFun();//-------->second
+(function (){
+    //devloper 1 
+    let secret = "asdfsadfsdf";
 
-var myFun = function (){
-    console.log("First");
-}
-
-myFun(); //-----------> First
+    console.log(secret);
+    
+})();
 
 
-function myFun(){
-    console.log("Second");
-}
 
-myFun(); //-----> First 
+//developer 2
+let secret = "1234";
+console.log(secret);
+
+
+//developer 1 doesnt want his secret variable to get polluted by the global scope 
+//so to prevent from global scope pollutoin he writes the secret insite a iife function 
+// and we all know that writting a iife function will execute the function immediately 
+
+
+((name)=>{
+    console.log(`My name is ${name}`);
+    
+})("sakib");
+
+
+((bon)=>{
+   
+     console.log(bon);
+    
+})("nazifa");
+
+
