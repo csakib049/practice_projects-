2630f748072bc0dadfe1f291f3a45324f3f231ad
Sakib <csakib049@gmail.com>
2026-09-03 23:24:37 +0600

    practicing

--- Javascript Practice/practice.js
+++ Javascript Practice/practice.js
@@ -39,3 +39,88 @@ console.log(secret);
     console.log(secret);
 })(); 
 
+
+
+(function (){
+    //devloper 1 
+    let secret = "asdfsadfsdf";
+
+    console.log(secret);
+    
+})();
+
+
+
+//developer 2
+let secret = "1234";
+console.log(secret);
+
+
+//developer 1 doesnt want his secret variable to get polluted by the global scope 
+//so to prevent from global scope pollutoin he writes the secret insite a iife function 
+// and we all know that writting a iife function will execute the function immediately 
+
+
+((name)=>{
+    console.log(`My name is ${name}`);
+    
+})("sakib");
+
+
+((bon)=>{
+   
+     console.log(bon);
+    
+})("nazifa");
+
+
+
+
+
+(function (){
+    let secret = 23;
+    console.log(secret);
+})(); 
+
+
+(function (){
+    //devloper 1 
+    let secret = "asdfsadfsdf";
+
+    console.log(secret);
+    
+})();
+
+
+
+//developer 2
+let secret = "1234";
+console.log(secret);
+
+
+//developer 1 doesnt want his secret variable to get polluted by the global scope 
+//so to prevent from global scope pollutoin he writes the secret insite a iife function 
+// and we all know that writting a iife function will execute the function immediately 
+
+
+((name)=>{
+    console.log(`My name is ${name}`);
+    
+})("sakib");
+
+
+((bon)=>{
+   
+     console.log(bon);
+    
+})("nazifa");
+
+
+
+
+
+(function (){
+    let secret = 23;
+    console.log(secret);
+})(); 
+
