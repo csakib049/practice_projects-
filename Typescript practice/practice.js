ae15631c73d37a187085302b89d6559630285acb
Sakib <csakib049@gmail.com>
2026-09-05 15:42:31 +0600

    tsc

---
+++ Typescript practice/practice.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 23.34;
+let username = "sakib";
+let collete = "BUBT";
+console.log(age);
+console.log(username);
+console.log(collete);
+let data = null;
+console.log(data);
+let a = 23;
+let direction;
+direction = "down";
+console.log(direction);
+//# sourceMappingURL=practice.js.map
---
+++ Typescript practice/practice.ts
@@ -0,0 +1,26 @@
+let age:number = 23.34;
+let username:string ="sakib";
+let collete :string= "BUBT";
+
+console.log(age);
+console.log(username);
+console.log(collete);
+
+
+let data : null = null;
+
+console.log(data);
+
+
+let a : unknown = 23;
+
+
+
+let direction :"up" | "down";
+
+direction = "down";
+
+console.log(direction);
+
+
+
