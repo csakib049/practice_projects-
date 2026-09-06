b30347c35174ebcb3fb257f95f5fc041c608ca97
Sakib <csakib049@gmail.com>
2026-09-06 19:50:53 +0600

    interface & generics

--- Typescript practice/practice.js
+++ Typescript practice/practice.js
@@ -1,20 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let sakib = null;
-console.log(sakib);
-let b = [1, 2, 34, 5];
-let c = ["asdf", "asdf", "fdas", 23];
-for (const cc of c)
-    console.log(cc);
-let user = [1, "asdf", true];
-for (const u of user)
-    console.log(u);
-let s = ["asdf", 3232];
-const user1 = {
-    name: "sakib",
-    age: 23,
-    address: "asdfsdf"
-};
-console.log(user1.name);
-let saki = 23;
+function datatype(item) {
+    console.log(item);
+}
+datatype("sakib");
+datatype(true);
+datatype(23);
+function pair(a, b) {
+    return [a, b];
+}
+pair("asdf", 23);
+pair(true, 23);
 //# sourceMappingURL=practice.js.map
--- Typescript practice/practice.ts
+++ Typescript practice/practice.ts
@@ -1,68 +1,29 @@
-let sakib : null | undefined =null  ;
-
-console.log(sakib);
-
-
-
-let b : number[]=[1,2,34,5];
-
-
-let c : (string | number)[]=["asdf","asdf","fdas", 23];
-
-
-for(const cc of c)console.log(cc);
+function datatype<T>(item:T):void{
+    console.log(item);
+    
+}
 
 
 
+datatype<string>("sakib");
 
 
-let user : [number, string , boolean] = [1,"asdf",true];
+datatype<boolean>(true);
 
-
-for(const u of user)console.log(u);
-
-
-
-let s:(string|number)[]=["asdf",3232];
+datatype<number>(23);
 
 
 
 
 
 
+function pair<A,B>(a:A,b:B):[A,B]{
+    return [a,b];
+}
 
 
 
+pair("asdf",23);
 
-type User={
-    name:string,
-    age:number,
-    address:string
-};
-
-
-const user1:User = {
-    name:"sakib",
-    age:23,
-    address:"asdfsdf"
-};
-
-
-console.log(user1.name);
-
-
-let saki:any = 23;
-
-
-
-
-
-
-
-
-
-
-
-
-
+pair(true,23);
 
