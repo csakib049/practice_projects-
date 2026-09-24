83994104bd4a129db2e6579b4594195ba1f720d4
Sakib <csakib049@gmail.com>
2026-09-24 22:08:19 +0600

    practicing vitest matchers

--- Typescript practice/vitest.test.ts
+++ Typescript practice/vitest.test.ts
@@ -1,27 +1,16 @@
 import { describe, it, expect } from "vitest";
-import { sum, isAdult } from './vitest';
+
+describe("Practicing ", () => {
+
+    function boom(){
+        throw new Error('Some thing is wrong');
+    }
 
 
-describe("function", () => {
-    it("should add 2 numbers", () => {
-        expect(sum(1, 2)).toBe(3);
-    });
+   
+    it("to be truthy",()=>{
+        expect(boom).toThrow();
+    })
 
+    
 });
-
-
-describe("Adult", () => {
-
-    it("should return true if input>18 ", () => {
-        expect(isAdult(19)).toBe(true);
-    })
-
-    it("should return true if input is 18",()=>{
-        expect(isAdult(18)).toBe(true);
-    })
-
-    it("shold return false if input<18",()=>{
-        expect(isAdult(17)).toBe(false);
-    })
-})
-
--- Typescript practice/vitest.ts
+++
@@ -1,8 +0,0 @@
-export function sum(a:number,b:number){
-    return a+b;
-}
-
-
-export function isAdult(age:number){
-    return age>=18
-}
